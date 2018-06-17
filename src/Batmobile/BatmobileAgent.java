0cab637f90da409e15bd6124f98a36b91bd661ef
Matheus Joranhezon <mjoranhezon@gmail.com>
2018-06-17 14:10:28 -0300

    Creating Utils packagea and also adding the skeleton of the Batmobile behaviours.

--- src/Batmobile/BatmobileAgent.java
+++ src/Batmobile/BatmobileAgent.java
@@ -1,5 +1,24 @@
 package Batmobile;
 
-public class BatmobileAgent {
+import jade.core.Agent;
+import Utils.Constants;
 
+public class BatmobileAgent extends Agent {
+	
+	private String pilot = null; 
+	private DriveBehavior driveBehavior;
+	private RendezvousBehavior rendezvousBehavior;
+	private ChangePilotBehavior changePilotBehavior;
+	private ReturnToBatcaveBehavior returnToBatcaveBehavior;
+	
+	/* Serves as the initializer of the class */
+	protected void ignition() {
+		this.pilot = Constants.CHANGE_PILOT_TO_BATMAN;
+		
+		driveBehavior = new DriveBehavior(this)
+		
+		insertYellowPages();
+		
+		addBehaviour(driveBehavior);
+	}
 }
---
+++ src/Utils/Constants.java
@@ -0,0 +1,6 @@
+package Utils;
+
+public final class Constants {
+	public final static String CHANGE_PILOT_TO_BATMAN = "Batman";
+	public final static String CHANGE_PILOT_TO_BATCOMPUTER = "Batcomputer";
+}
